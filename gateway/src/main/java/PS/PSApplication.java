8c1f065aad6b5d7d264b09706eb55ad8f5265095
Alex Sawicki <tul69540@temple.edu>
2022-09-16 14:28:51 -0400

    created setup files needed for springboot application

---
+++ gateway/src/main/java/PS/PSApplication.java
@@ -0,0 +1,13 @@
+package PS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(PS.PSApplication.class, args);
+    }
+
+}
