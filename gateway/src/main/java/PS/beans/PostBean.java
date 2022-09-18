2def9085a51a854ee96a4ba01b3177749a982c5c
Alex Sawicki <tul69540@temple.edu>
2022-09-18 14:26:39 -0400

    PS-4 adding properties to application.properties, updated pom.xml to include dependency to allow mysql connection, added data fields for PostBean.java

--- gateway/src/main/java/PS/beans/PostBean.java
+++ gateway/src/main/java/PS/beans/PostBean.java
@@ -1,5 +1,6 @@
 package PS.beans;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,7 +13,24 @@ public class PostBean {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="post_id")
     protected int post_id;
 
+    @Column(name="publishdate")
+    protected String publishdate;
 
+    @Column(name="title")
+    protected String title;
+
+    @Column(name="url")
+    protected String url;
+
+    @Column(name="content")
+    protected String content;
+
+    @Column(name="isprivate")
+    protected boolean isprivate;
+
+    @Column(name="creator")
+    protected String creator;
 }
