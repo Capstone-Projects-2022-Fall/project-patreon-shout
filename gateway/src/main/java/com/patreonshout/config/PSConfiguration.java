9694361dc9b83bcb377b6c18a3357809d504a1b1
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-09-26 11:26:19 -0400

    PS-83: Added Patreon OAuth related Spring components

--- gateway/src/main/java/com/patreonshout/config/PSConfiguration.java
+++ gateway/src/main/java/com/patreonshout/config/PSConfiguration.java
@@ -1,5 +1,8 @@
 package com.patreonshout.config;
 
+import com.patreon.PatreonOAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,4 +12,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Import({JPAConfiguration.class})
 public class PSConfiguration {
 
+	@Value("${patreonshout.client.id}") String clientId;
+	@Value("${patreonshout.client.secret}") String clientSecret;
+	@Value("${patreonshout.client.redirecturi}") String redirectUri;
+
+	@Bean
+	public PatreonOAuth oauthClient() {
+		return new PatreonOAuth(clientId, clientSecret, redirectUri);
+	}
 }
