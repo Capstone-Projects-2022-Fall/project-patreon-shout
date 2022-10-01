52a9db0f7a74263b697c0c6e91b53dd3e785ef29
Christian Saputro <tui01777@temple.edu>
2022-09-30 20:30:42 -0400

    PS-80: updated input bar, for a better UI

--- frontend/loginform/src/components/loginform.js
+++ frontend/loginform/src/components/loginform.js
@@ -6,37 +6,44 @@ import GoogleIcon from '@mui/icons-material/Google';
 
 const LoginForm = () => {
 
+    const [userName, setUserName] = useState('')
+    const [userPassword, setPassword] = useState('')
+
+
     //shows the login failed if username/pass != correct
     const popup = () => {
         showPopup("loginPop")
-        setTimeout(() => showPopup("hide"), 5000)
+        setTimeout(() => showPopup("hide"), 4000)
     }
 
     //hides the login failed warning in general
     const [popupFailed, showPopup] = useState("hide")
 
     return(
-        <div className = "cover">
-            <h1>Sign in</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="Password" />
+    <div className="cover">
+        <h1>Sign in</h1>
+        <TextField className='textInputLog' id="outlined-basic" label="Username" variant="outlined"
+                   value={userName} onChange={(e) => setUserName(e.target.value)}/>
 
-            <div className="loginButton" onClick={popup}>Sign in</div>
+        <TextField className='textInputLog' id="outlined-basic" label="Password" type='password'
+                   variant="outlined" value={userPassword} onChange={(e) => setPassword(e.target.value)}/>
 
-            <p className="text">Or sign in with with</p>
+        <div className="loginButton" onClick={popup}>Sign in</div>
 
-            <div className="OtherLogin">
+        <p className="text">Or sign in with with</p>
+
+        <div className="OtherLogin">
             <TwitterIcon></TwitterIcon>
             <FacebookIcon></FacebookIcon>
             <GoogleIcon></GoogleIcon>
-            </div> 
+        </div>
 
-            <div className={popupFailed}>
+        <div className={popupFailed}>
             <h3>Sign in Failed</h3>
             <p>Incorrect Username or Password. Please try again</p>
 
         </div>
-        </div>
+    </div>
     )
 }
 
