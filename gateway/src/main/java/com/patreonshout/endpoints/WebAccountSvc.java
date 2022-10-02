0a1ce00cfdb7135290c5fe4adceba9d775b2b3aa
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-10-02 18:20:27 -0400

    PS-91: Implemented Swagger Open API v3 to Endpoint Reachable when running locally at http://localhost:5000/swagger-ui/index.html#/

--- gateway/src/main/java/com/patreonshout/endpoints/WebAccountSvc.java
+++ gateway/src/main/java/com/patreonshout/endpoints/WebAccountSvc.java
@@ -1,21 +1,18 @@
 package com.patreonshout.endpoints;
 
+import com.patreonshout.interfaces.WebAccountImpl;
 import com.patreonshout.jpa.WebAccount;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/v1")
-public class WebAccountSvc {
+public class WebAccountSvc implements WebAccountImpl {
 
 	@Autowired
 	WebAccount webAccount;
 
 	// This String object will contain an error message, or be empty if no error occurred
-	@GetMapping("/register")
 	public String Register(
 			@RequestParam(name = "user") String username,
 			@RequestParam(name = "pass") String password
