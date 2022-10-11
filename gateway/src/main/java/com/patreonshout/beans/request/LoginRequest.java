fe23ab130a7e4ea0a0c27b178d13b1b0c4a8a73f
Alex Sawicki <tul69540@temple.edu>
2022-10-11 13:05:52 -0400

    adding javadocs to loginrequest

--- gateway/src/main/java/com/patreonshout/beans/request/LoginRequest.java
+++ gateway/src/main/java/com/patreonshout/beans/request/LoginRequest.java
@@ -5,13 +5,23 @@ import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ *  Jackson Serialized object, used when a login request is received
+ */
+
 @Getter
 public class LoginRequest {
 
+	/**
+	 * The desired username for the given request
+	 */
 	@NotNull
 	@JsonProperty("user")
 	private String username;
 
+	/**
+	 * The desired password for the given request
+	 */
 	@NotNull
 	@JsonProperty("pass")
 	private String password;
