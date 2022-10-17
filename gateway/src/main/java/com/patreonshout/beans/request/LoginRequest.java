ae2d155902a41109fa1d56586b94ea7731cae1d4
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-10-17 18:05:04 -0400

    PS-96: LoginRequest now implements Serializable for Jackson

--- gateway/src/main/java/com/patreonshout/beans/request/LoginRequest.java
+++ gateway/src/main/java/com/patreonshout/beans/request/LoginRequest.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 /**
  *  Jackson Serialized object, used when a login request is received
  */
-
 @Getter
-public class LoginRequest {
+public class LoginRequest implements Serializable {
 
 	/**
 	 * The desired username for the given request
