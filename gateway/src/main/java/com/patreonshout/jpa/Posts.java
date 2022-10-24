dacd11c7b39276c525aa39f0eea5f96f479868da
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-10-24 11:13:28 -0400

    PS-56: Improved Posts class documentation, changed getCreatorPosts to require a creator page url

--- gateway/src/main/java/com/patreonshout/jpa/Posts.java
+++ gateway/src/main/java/com/patreonshout/jpa/Posts.java
@@ -8,12 +8,13 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 /**
- * Posts Repository Wrapper
- *
+ * Posts Repository Wrapper <br/><br/>
  * <p>
- *     Responsibilities:
- *     1) Be a wrapper class for {@link com.patreonshout.jpa.PostsRepository}
- *     2) Add any logic needed before going into {@link com.patreonshout.jpa.PostsRepository}
+ *     <b>Responsibilities</b>
+ *     <ol>
+ *         <li>Be a wrapper class for {@link com.patreonshout.jpa.PostsRepository}</li>
+ *         <li>Add any logic needed before going into {@link com.patreonshout.jpa.PostsRepository}</li>
+ *     </ol>
  * </p>
  */
 @Component
@@ -27,17 +28,17 @@ public class Posts {
     PostsRepository postsRepository;
 
     /**
-     * getCreatorPosts() gets the posts from a particular creator
+     * Gets the posts from a particular creator page
      *
-     * @param creator is the creator who made the posts we want to get
-     * @return a List of {@link com.patreonshout.beans.PostBean} objects containing Patreon post information of a given creator
+     * @param creator_page_url the creator's url/handle
+     * @return a List of {@link com.patreonshout.beans.PostBean} objects containing Patreon post information of a given creator_page_url
      */
-    public List<PostBean> getCreatorPosts(String creator) {
-        return postsRepository.getCreatorPosts(creator);
+    public List<PostBean> getCreatorPosts(String creator_page_url) {
+        return postsRepository.getCreatorPosts(creator_page_url);
     }
 
     /**
-     * getPost() gets a specific post based on the post url
+     * Gets a specific post based on the post url
      *
      * @param url is the url of the Patreon post
      * @return a {@link com.patreonshout.beans.PostBean} object containing the Patreon post information of the given url
@@ -47,7 +48,7 @@ public class Posts {
     }
 
     /**
-     * getAllPosts() returns every post in the database
+     * Gets every post in the database
      *
      * @return a List of {@link com.patreonshout.beans.PostBean} containing every post in the database
      */
@@ -56,7 +57,7 @@ public class Posts {
     }
 
     /**
-     * putPost() adds a new Post to the database
+     * Adds a new Post to the database
      *
      * @param pb is the post to be added to the database
      */
@@ -65,7 +66,7 @@ public class Posts {
     }
 
     /**
-     * updatePost() updates an existing post in the database based on the post_id
+     * Updates an existing post in the database based on the post_id
      *
      * @param pb is the post to be updated in the database
      */
@@ -74,7 +75,7 @@ public class Posts {
     }
 
     /**
-     * removePost() removes a post in the database based on the post url
+     * Removes a post in the database based on the post url
      *
      * @param url is the url of the Patreon post
      */
