084a5470901739149c8c1c63ce6d8155dc4f9aad
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-10-24 11:13:01 -0400

    PS-56: Changed PostBean to use Lombok

--- gateway/src/main/java/com/patreonshout/beans/PostBean.java
+++ gateway/src/main/java/com/patreonshout/beans/PostBean.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.jasminb.jsonapi.annotations.Type;
 import com.patreon.resources.shared.BaseResource;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -19,6 +22,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name="posts")
 @Type("post")
+@Setter
+@Getter
+@ToString
 public class PostBean extends BaseResource {
 
     /**
@@ -69,133 +75,4 @@ public class PostBean extends BaseResource {
     @Column(name="is_public")
     @JsonProperty("is_public")
     protected boolean is_public;
-
-    /**
-     * post_id getter
-     * @return post_id
-     */
-    public int getPost_id() {
-        return post_id;
-    }
-
-    /**
-     * post_id setter
-     * @param post_id
-     */
-    public void setPost_id(int post_id) {
-        this.post_id = post_id;
-    }
-
-    /**
-     * publishdate getter
-     * @return publishdate
-     */
-    public String getPublishdate() {
-        return publishdate;
-    }
-
-    /**
-     * publishdate setter
-     * @param publishdate
-     */
-    public void setPublishdate(String publishdate) {
-        this.publishdate = publishdate;
-    }
-
-    /**
-     * title getter
-     * @return title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * title setter
-     * @param title
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    /**
-     * url getter
-     * @return url
-     */
-    public String getUrl() {
-        return url;
-    }
-
-    /**
-     * url setter
-     * @param url
-     */
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    /**
-     * content getter
-     * @return content
-     */
-    public String getContent() {
-        return content;
-    }
-
-    /**
-     * content setter
-     * @param content
-     */
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    /**
-     * is_public getter
-     * @return is_public
-     */
-    public boolean isIs_public() {
-        return is_public;
-    }
-
-    /**
-     * is_public setter
-     * @param is_public
-     */
-    public void setIs_public(boolean is_public) {
-        this.is_public = is_public;
-    }
-
-    /**
-     * creator getter
-     * @return creator
-     */
-    public String getCreator() {
-        return creator_page_url;
-    }
-
-    /**
-     * creator setter
-     * @param creator_page_url
-     */
-    public void setCreator(String creator_page_url) {
-        this.creator_page_url = creator_page_url;
-    }
-
-    /**
-     * toString method to have ease of access to data
-     * @return all data fields inside of a formatted string
-     */
-    @Override
-    public String toString() {
-        return "PostBean{" +
-                "post_id=" + post_id +
-                ", publishdate='" + publishdate + '\'' +
-                ", title='" + title + '\'' +
-                ", url='" + url + '\'' +
-                ", content='" + content + '\'' +
-                ", is_public=" + is_public +
-                ", creator_page_url='" + creator_page_url + '\'' +
-                '}';
-    }
 }
