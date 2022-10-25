327238f6716f2e18377a1da95a82e2c078cb87a2
Jonah Malriat <tuf73590@temple.edu>
2022-10-25 17:41:12 -0400

    Some searchbar work done

--- frontend/src/home_page/Searchbar.js
+++ frontend/src/home_page/Searchbar.js
@@ -1,26 +1,39 @@
 import { Button } from "@mui/material";
+import { SearchRounded } from "@mui/icons-material";
 import React, { useState } from "react";
 import "./home_css/Searchbar.css";
 
 function Searchbar() {
-  const [searchInput, setSearch] = useState("");
-  return (
-    <div className="Searchbar">
-      <form>
-        <div className="Searchbar__input">
-          <input
-          value={searchInput}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="Search PatreonShout"
-          type="text"
-          />
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value);
+      };
+
+//    if (searchInput.length > 0) {
+//        posts.filter((post) => {
+//        return post.name.match(searchInput);
+//        });
+//    }
+
+    const [searchInput, setSearch] = useState("");
+    return (
+        <div className="Searchbar">
+            <form>
+                <div className="Searchbar__input">
+                    <input
+                        value={searchInput}
+                        placeholder="Search PatreonShout..."
+                        onChange={handleChange}
+                        type="text"
+                    />
+                    <Button type="submit" className="Searchbar__button">
+                        <SearchRounded/>
+                    </Button>
+                </div>
+            </form>
         </div>
-        <Button type="submit" className="Searchbar__button">
-          Search
-        </Button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Searchbar;
