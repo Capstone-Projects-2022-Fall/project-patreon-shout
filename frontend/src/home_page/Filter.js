975a2ca7439ad4094f6f97c9ea7e742c144d3af9
Jonah Malriat <tuf73590@temple.edu>
2022-10-27 16:55:37 -0400

    Beginning work on Filter tool

---
+++ frontend/src/home_page/Filter.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import Popup from "reactjs-popup";
+import { Button } from "@mui/material";
+import "./home_css/Feed.css";
+
+function Filter() {
+
+    const [selection, setSelection] = useState("We can see it");
+
+    return (
+        <Popup 
+        trigger={<div className="dropdown">{selection}</div>}
+        position="bottom"
+        on="click"
+        contentStyle={{ padding: '0px', border: 'none' }}>
+            <div className="menu">
+                <div className="menu-item"> item 1</div>
+                <div className="menu-item"> item 2</div>
+                <div className="menu-item"> item 3</div>      
+            </div>
+        </Popup>
+    );
+}
+export default Filter
