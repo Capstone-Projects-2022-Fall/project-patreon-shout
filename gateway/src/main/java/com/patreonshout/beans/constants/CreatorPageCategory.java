c8fb04cc5529a76812c1aab6e691be0328179622
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-10-26 20:05:18 -0400

    PS-56: Created entity for creator_pages database table

---
+++ gateway/src/main/java/com/patreonshout/beans/constants/CreatorPageCategory.java
@@ -0,0 +1,38 @@
+package com.patreonshout.beans.constants;
+
+public enum CreatorPageCategory {
+	/**
+	 * Video production/Youtuber
+	 */
+	VIDEO,
+
+	/**
+	 * Podcasts
+	 */
+	PODCASTS,
+
+	/**
+	 * Music
+	 */
+	MUSIC,
+
+	/**
+	 * Comics
+	 */
+	COMICS,
+
+	/**
+	 * Writing
+	 */
+	WRITING,
+
+	/**
+	 * Gaming
+	 */
+	GAMES,
+
+	/**
+	 * Miscellaneous
+	 */
+	OTHER
+}
