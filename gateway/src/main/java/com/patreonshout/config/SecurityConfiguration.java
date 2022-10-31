14a9a94edd83849486a8ec7e3a68bdd6b55ca0f8
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-10-30 20:24:59 -0400

    PS-102: Added password requirement validation to SecurityConfiguration

--- gateway/src/main/java/com/patreonshout/config/SecurityConfiguration.java
+++ gateway/src/main/java/com/patreonshout/config/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package com.patreonshout.config;
 import com.patreonshout.jpa.WebAccountFunctions;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.passay.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 /**
  * Spring Security Configuration
@@ -17,6 +19,28 @@ import java.security.SecureRandom;
 public class SecurityConfiguration {
 
 	SecureRandom secureRandom = new SecureRandom();
+
+	/**
+	 * Set of requirements a password must meet to be considered valid
+	 */
+	PasswordValidator passwordValidator = new PasswordValidator(Arrays.asList(
+			// Must be at least 8 and at most 255 characters
+			new LengthRule(8, 255),
+
+			// Require at least one uppercase character
+			new CharacterRule(EnglishCharacterData.UpperCase, 1),
+
+			// Require at least one lowercase character
+			new CharacterRule(EnglishCharacterData.LowerCase, 1),
+
+			// Require at least one digit
+			new CharacterRule(EnglishCharacterData.Digit, 1),
+
+			// Require at least one symbol (special character)
+			new CharacterRule(EnglishCharacterData.Special, 1),
+
+			// No spaces allowed
+			new WhitespaceRule()));
 
 	/**
 	 * Pepper value for WebAccount passwords -- used as a constant value included in every password
@@ -51,7 +75,7 @@ public class SecurityConfiguration {
 	 * Checks if a given password and salt combine to the given encoded password.
 	 * This function utilizes the "pepper" constant that is assigned in the application properties.
 	 *
-	 * @param rawPassword {@link String} raw password retrieved from user input
+	 * @param rawPassword     {@link String} raw password retrieved from user input
 	 * @param encodedPassword {@link String} Encoded password retrieved from a {@link WebAccountFunctions} in the database
 	 * @return {@link Boolean} true if password matches, false otherwise
 	 */
@@ -70,5 +94,24 @@ public class SecurityConfiguration {
 		secureRandom.nextBytes(salt);
 
 		return Hex.encodeHexString(salt);
+	}
+
+	/**
+	 * Checks if the given raw password fits our password requirements.<br/>
+	 * <br/>
+	 * <b>Requirements</b>
+	 * <ul>
+	 *     <li>At least one uppercase character</li>
+	 *     <li>At least one lowercase character</li>
+	 *     <li>At least one digit</li>
+	 *     <li>At least one symbol (special character)</li>
+	 *     <li>No spaces</li>
+	 * </ul>
+	 *
+	 * @param rawPassword password to check
+	 * @return {@link Boolean#TRUE} if password meets our password requirements, {@link Boolean#FALSE} otherwise
+	 */
+	public boolean passwordIsValid(String rawPassword) {
+		return passwordValidator.validate(new PasswordData(rawPassword)).isValid();
 	}
 }
