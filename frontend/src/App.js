90882265462b7c04632bd22b69f329c1d757c688
Alex Sawicki <tul69540@temple.edu>
2022-10-31 17:05:06 -0400

    PS-105 add explore page for implementation in future tickets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./home_page/Home";
+import Explore from "./explore_page/Explore";
 import LoginPage from "./login_page/LoginPage";
 import ListPage from "./list_page/ListPage";
 import SettingsPage from "./settings/SettingsContainers"
@@ -27,6 +28,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage setToken={setToken}/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/explore" element={<Explore/>}/>
         <Route path="/lists" element={<ListPage/>}/>
         <Route path="/settings" element={<SettingsPage/>}/>
       </Routes>
