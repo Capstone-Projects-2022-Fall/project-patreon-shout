04f604de4f806493c5258e19370daa2b85426b81
Alex Sawicki <tul69540@temple.edu>
2022-11-01 13:38:57 -0400

    PS-100 reset password works

--- frontend/src/settings/ResetPassModal.js
+++ frontend/src/settings/ResetPassModal.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Stack, Box, Typography, Button, Modal, TextField } from '@mui/material';
+import PasswordChecklist from "react-password-checklist";
+import {resetPassword} from "../services/api/resetpass";
 
 const style = {
     position: 'absolute',
@@ -15,9 +17,48 @@ const style = {
 
 function ResetPassModal() {
 
+    const [popup, showPopup] = React.useState("hide")
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const [curPass, setCurPass] = React.useState("");
+    const [newPass, setNewPass] = React.useState("");
+    const [sameNewPass, setSameNewPass] = React.useState("");
+    let passIsValid;
+
+    const handleSubmit = async e => {
+
+        if (passIsValid === false) {
+            document.getElementById("reset").textContent = "Register Failed";
+            document.getElementById("errormsg").textContent = "Password does not meet the requirements";
+            showPopup("resetPop")
+            return 1;
+        }
+
+        let current_password = curPass;
+        let new_password = newPass;
+        const tokenString = localStorage.getItem('token');
+        const login_token = JSON.parse(tokenString).token;
+
+        const retVal = await resetPassword({
+            login_token,
+            current_password,
+            new_password
+        })
+
+        console.log(retVal);
+
+        if (retVal.success === "true") {
+            document.getElementById("reset").textContent = "Register Success";
+            document.getElementById("errormsg").textContent = "";
+        } else {
+            document.getElementById("reset").textContent = "Register Failed";
+            document.getElementById("errormsg").textContent = retVal.status + ": " + retVal.message;
+        }
+        showPopup("resetPop")
+    }
+
 
     return (
         <div>
@@ -38,23 +79,46 @@ function ResetPassModal() {
                     </Typography>
 
                     <Typography component="h2" sx={{ mt: 3 }}>
-                        <TextField fullWidth id="outlined-basic" label="Current Password" type="password" variant="outlined" />
+                        <TextField fullWidth id="outlined-basic" label="Current Password" type="password" variant="outlined" value={curPass} onChange={(e) => setCurPass(e.target.value)}/>
                     </Typography>
 
                     <Typography component="h2">
-                        <TextField fullWidth id="outlined-basic" label="New Password" type="password" variant="outlined" />
+                        <TextField fullWidth id="outlined-basic" label="New Password" type="password" variant="outlined" value={newPass} onChange={(e) => setNewPass(e.target.value)}/>
                     </Typography>
 
                     <Typography component="h2">
-                        <TextField fullWidth id="outlined-basic" label="Confirm New Password" type="password" variant="outlined" />
+                        <TextField fullWidth id="outlined-basic" label="Confirm New Password" type="password" variant="outlined" value={sameNewPass} onChange={(e) => setSameNewPass(e.target.value)}/>
                     </Typography>
+
+                    <PasswordChecklist
+                       rules={["minLength","capital","lowercase","number","specialChar"]}
+                        minLength={8}
+                        value={newPass}
+                        iconSize={14}
+                        messages={{
+                            minLength: "At least 8 characters",
+                            capital: "At least one uppercase letter",
+                            lowercase: "At least one lowercase letter",
+                            number: "At least one number",
+                            specialChar: "At least one special character",
+                        }}
+                        onChange={(passIsValid) => {}}
+                        className="passwordRequirements"
+                    />
 
                     <Box sx={{ flexGrow: 1 }} mt={2}>
                         <Stack direction="row" spacing={21}>
                             <Button variant="text" color='error' onClick={handleClose}>cancel</Button>
-                            <Button variant="contained">Submit</Button>
+                            <Button variant="contained" onClick={() => {
+                                handleSubmit();
+                            }}>Submit</Button>
                         </Stack>
                     </Box>
+
+                    <div className={popup}>
+                        <h3 id="reset"> </h3>
+                        <p id="errormsg"> </p>
+                    </div>
                 </Box>
             </Modal>
         </div>
