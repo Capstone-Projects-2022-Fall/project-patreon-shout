730b9f2fcfaa01ed64bd4b157d0c577b250c369c
Alex Sawicki <tul69540@temple.edu>
2022-11-05 18:51:24 -0400

    PS-119 addListModal now only has "Title" input, "Description" input, and "Create" button

--- frontend/src/list_page/AddListModal.js
+++ frontend/src/list_page/AddListModal.js
@@ -1,52 +1,54 @@
 import React, {useState} from "react";
 import Popup from "reactjs-popup";
 import PostAddIcon from "@mui/icons-material/PostAdd";
-import Creator from "../components/Creator";
 import "./list_css/AddListModal.css";
 import {addList} from "../services/api/lists/addList";
 import jsonCreators from "../data/creators.json";
+import {Button} from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 
 function AddListModal() {
 
+
+
     // showing the options of creators
-    const [stableList] = useState(jsonCreators);
-    const [displayedList, setDisplayedList] = useState(jsonCreators);
+    // const [stableList] = useState(jsonCreators);
+    // const [displayedList, setDisplayedList] = useState(jsonCreators);
 
     // adding creators to a list
     const [creatorList, setCreatorList] = useState([]);
     const [titleInput, setTitle] = useState("");
     const [descInput, setDesc] = useState("");
-    const [searchInput, setSearch] = useState("");
+    // const [searchInput, setSearch] = useState("");
 
-    const searchCreators = (value) => {
-        let filteredList = [];
-        let added = false;
-
-        stableList.forEach((creator) => {
-            if (creator.displayName.toLowerCase().replaceAll(" ", "").includes(value.replaceAll(" ", "")) && added === false) {
-                filteredList.push(creator);
-                added = true;
-            }
-            if (creator.urlName.toLowerCase().replaceAll(" ", "").includes(value.replaceAll(" ", "")) && added === false) {
-                filteredList.push(creator);
-                added = true;
-            }
-            if (creator.description.toLowerCase().replaceAll(" ", "").includes(value.replaceAll(" ", "")) && added === false) {
-                filteredList.push(creator);
-                added = true;
-            }
-            added = false;
-        });
-
-        setDisplayedList(filteredList);
-    }
+    // const searchCreators = (value) => {
+    //     let filteredList = [];
+    //     let added = false;
+    //
+    //     stableList.forEach((creator) => {
+    //         if (creator.displayName.toLowerCase().replaceAll(" ", "").includes(value.replaceAll(" ", "")) && added === false) {
+    //             filteredList.push(creator);
+    //             added = true;
+    //         }
+    //         if (creator.urlName.toLowerCase().replaceAll(" ", "").includes(value.replaceAll(" ", "")) && added === false) {
+    //             filteredList.push(creator);
+    //             added = true;
+    //         }
+    //         if (creator.description.toLowerCase().replaceAll(" ", "").includes(value.replaceAll(" ", "")) && added === false) {
+    //             filteredList.push(creator);
+    //             added = true;
+    //         }
+    //         added = false;
+    //     });
+    //
+    //     setDisplayedList(filteredList);
+    // }
 
     const eraseInfo = () => {
         setCreatorList([]);
         setTitle("");
         setDesc("");
-        setSearch("");
     }
 
     const handleAddList = async e => {
@@ -94,48 +96,38 @@ function AddListModal() {
                         Create a new List
                     </div>
 
-                    <form id="addListForm">
-                        <input
-                            value={titleInput}
-                            onChange={(e) => setTitle(e.target.value)}
-                            placeholder="Title"
-                            type="text"
-                        />
-                        <input
-                            value={descInput}
-                            onChange={(e) => setDesc(e.target.value)}
-                            placeholder="Description"
-                            type="text"
-                        />
-                        <input
-                            value={searchInput}
-                            onChange={(e) => {setSearch(e.target.value); searchCreators(e.target.value);}}
-                            placeholder="Search Creators"
-                            type="text"
-                        />
-                        <input type="button" value="Create List" onClick={() => {
-                            const added = handleCreateList();
-                            if (added === true) {
-                                close();
-                            }
-                            else {
-                                console.log("add list error msg");
-                            }
-                        }}/>
-                    </form>
-
-                    {displayedList.map((item) => (
-                        <Creator
-                            addedState={false}
-                            curCreatorList={creatorList}
-                            setCreatorList={setCreatorList}
-                            displayName={item.displayName}
-                            urlName={item.urlName}
-                            description={item.description}
-                            imgUrl={item.imgUrl}
-                            verified={item.verified}
-                        />
-                    ))}
+                    {/*<form id="addListForm">*/}
+                    {/*    <input*/}
+                    {/*        value={titleInput}*/}
+                    {/*        onChange={(e) => setTitle(e.target.value)}*/}
+                    {/*        placeholder="Title"*/}
+                    {/*        type="text"*/}
+                    {/*    />*/}
+                    {/*    <input*/}
+                    {/*        value={descInput}*/}
+                    {/*        onChange={(e) => setDesc(e.target.value)}*/}
+                    {/*        placeholder="Description"*/}
+                    {/*        type="text"*/}
+                    {/*    />*/}
+                    {/*    <input type="button" value="Create List" onClick={() => {*/}
+                    {/*        const added = handleCreateList();*/}
+                    {/*        if (added === true) {*/}
+                    {/*            close();*/}
+                    {/*        }*/}
+                    {/*        else {*/}
+                    {/*            console.log("add list error msg");*/}
+                    {/*        }*/}
+                    {/*    }}/>*/}
+                    {/*</form>*/}
+                        <div className="fields">
+                            <TextField id="outlined-basic" label={"Title"} size={"small"} variant="outlined" value={titleInput} onChange={(e) => setTitle(e.target.value)}/>
+                            <TextField id="outlined-basic" label={"Description"} size={"small"} variant="outlined" value={descInput} onChange={(e) => setDesc(e.target.value)}/>
+                            <div id="buttonContainer">
+                                <Button id="submitbtn" sx={ { borderRadius: 10 } } color="primary" type="submit" variant="contained"  onClick={() => {console.log("hello");}}>
+                                    Create
+                                </Button>
+                            </div>
+                        </div>
                 </div>
             )}
         </Popup>
