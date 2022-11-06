8bbf72b783bafb8adc6272a78749cc2cfde09fce
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-11-06 09:09:45 -0500

    PS-63: Improved PatreonDataV2 documentation

--- gateway/src/main/java/com/patreonshout/beans/patreon_api/PatreonDataV2.java
+++ gateway/src/main/java/com/patreonshout/beans/patreon_api/PatreonDataV2.java
@@ -6,11 +6,16 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * POJO that represents the "Data" object returned by Patreon API V2
+ */
 @Data
 public class PatreonDataV2 implements Serializable {
 
 	/**
-	 * An anonymous object that
+	 * An anonymous object that, when sent from Patreon API V2, can (seemingly) be any Patreon V2 object except
+	 * {@link PatreonDataV2}
+	 *
 	 * This object <b>must</b> be converted to the desired object via {@link ObjectMapper}.
 	 */
 	@JsonProperty("attributes")
