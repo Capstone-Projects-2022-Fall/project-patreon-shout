76aaaf6b1770873e5cbc6734f5ec5fd354f5d1b3
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-11-08 19:53:33 -0500

    PS-106: Creator Settings "save" button dynamically changes based off of social integration HTTP status

--- frontend/src/settings/DropDownListItem.js
+++ frontend/src/settings/DropDownListItem.js
@@ -2,12 +2,19 @@ import React, {useState} from 'react';
 import {Button, ListItem, ListItemText, Switch} from '@mui/material';
 import './setting_css/DropDownListItem.css';
 import TextField from '@mui/material/TextField';
+import PendingIcon from '@mui/icons-material/Pending';
+import ErrorIcon from '@mui/icons-material/Error';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import SaveIcon from '@mui/icons-material/Save';
 import {putSocialIntegration} from "../services/api/webaccount/putSocialIntegration";
 
 function DropDownListItem({name, textfieldLabel, dropdown, defaultChecked, defaultValue, isDisabled}) {
     const [checked, setChecked] = useState(defaultChecked);
     const [showDropdown, setShowDropdown] = useState(defaultChecked && dropdown ? "show" : "hide");
     const [textfieldValue, setTextfieldValue] = useState(defaultValue);
+    const [buttonColor, setButtonColor] = useState("primary");
+    const [buttonIcon, setButtonIcon] = useState(<SaveIcon/>);
+    const [isProcessing, setIsProcessing] = useState(null);
 
     // Button and switch toggling
     const handleToggle = () => () => {
@@ -43,22 +50,38 @@ function DropDownListItem({name, textfieldLabel, dropdown, defaultChecked, defau
                         />
                         <Button
                             sx={{borderRadius: 10}}
-                            color="primary"
+                            color={buttonColor}
                             type="submit"
                             variant="contained"
                             size={"medium"}
-                            disabled={isDisabled || !textfieldValue}
-                            onClick={async () => {
-                                const tokenString = localStorage.getItem('token');
-                                const message = await putSocialIntegration({
-                                    login_token: JSON.parse(tokenString).token,
-                                    integration_name: name.toUpperCase(),
-                                    data: textfieldValue
-                                })
+                            disabled={isDisabled || isProcessing}
+                            endIcon={buttonIcon}
+                            onClick={
+                                async () => {
+                                    if (isProcessing)
+                                        return;
 
-                                console.log(message)
-                            }}>
-                            Submit
+                                    setIsProcessing(true);
+                                    setButtonIcon(<PendingIcon/>);
+
+                                    const tokenString = localStorage.getItem('token');
+                                    const retVal = await putSocialIntegration({
+                                        login_token: JSON.parse(tokenString).token,
+                                        integration_name: name.toUpperCase(),
+                                        data: textfieldValue
+                                    })
+
+                                    if (retVal.status === 200) {
+                                        setButtonColor("success");
+                                        setButtonIcon(<CheckCircleIcon/>);
+                                    } else {
+                                        setButtonColor("error");
+                                        setButtonIcon(<ErrorIcon/>);
+                                    }
+
+                                    setIsProcessing(false);
+                                }}>
+                            Save
                         </Button>
                     </div>
                 </div>
