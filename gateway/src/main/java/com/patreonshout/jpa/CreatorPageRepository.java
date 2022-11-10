810e66e6908ae3a1e0a111b9704a8353ee6b1584
Alex Sawicki <tul69540@temple.edu>
2022-11-10 00:41:28 -0500

    PS-119 fixing main issues

--- gateway/src/main/java/com/patreonshout/jpa/CreatorPageRepository.java
+++ gateway/src/main/java/com/patreonshout/jpa/CreatorPageRepository.java
@@ -7,11 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CreatorPageRepository extends JpaRepository<CreatorPage, Long> {
 
-	/**
-	 * find a creator page by their page url
-	 *
-	 * @param pageUrl the page url of the creator
-	 * @return the {@link com.patreonshout.beans.CreatorPage} linked to the page url
-	 */
-	CreatorPage findByPageUrl(String pageUrl);
 }
