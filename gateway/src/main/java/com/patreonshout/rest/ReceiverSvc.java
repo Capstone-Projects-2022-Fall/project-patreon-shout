858a9eaef3e66285a70ad93b4a3155135dd63fab
Alex Sawicki <tul69540@temple.edu>
2022-11-13 20:09:08 -0500

    PS-123 post body refactor

--- gateway/src/main/java/com/patreonshout/rest/ReceiverSvc.java
+++ gateway/src/main/java/com/patreonshout/rest/ReceiverSvc.java
@@ -372,7 +372,7 @@ public class ReceiverSvc extends BaseSvc implements ReceiverImpl {
 
 	void sendTwitterPost(PatreonPostV2 patreonPost, WebAccount webAccount) throws PSException {
 		SocialIntegration socialIntegration = webAccount.getSocialIntegration();
-		String body = "I just made a new Patreon Post!\n" + patreonPost.getTitle() + ": " + (patreonPost.getIsPublic() ? patreonPost.getContent() : "This post is private, go to the post to see more");
+		String body = "I just made a new Patreon Post!\n" + patreonPost.getTitle() + ": " + (patreonPost.getIsPublic() ? patreonPost.getContent().replaceAll("<p>", "").replaceAll("</p>", "") : "This post is private, go to the post to see more") + "\nhttps://www.patreon.com" + patreonPost.getUrl();
 
 		new TwitterApiUtil().sendTweet(twitterCredentials.getClientID(), twitterCredentials.getClientSecret(), socialIntegration.getTwitterAccessToken(), socialIntegration.getTwitterRefreshToken(), body);
 	}
