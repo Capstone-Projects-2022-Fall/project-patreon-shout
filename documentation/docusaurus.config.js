5cd8d9c1efa721156088d901f559f66e5ded7e68
Jonah Malriat <Jonahmalriat@gmail.com>
2022-11-17 16:28:03 -0500

    Update docusaurus.config.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
  * The URL or reference to your projects logo!
  * @type {string}
  */
-const logo = 'https://upload.wikimedia.org/wikipedia/commons/1/17/Temple_T_logo.svg';
+const logo = 'https://github.com/Capstone-Projects-2022-Fall/project-patreon-shout/blob/Jsmalriat-doc-patches/documentation/megaphone.png';
 
 const main_template_jira_scripts = () => {
   if (process.env.ORG_NAME === 'ApplebaumIan'){
