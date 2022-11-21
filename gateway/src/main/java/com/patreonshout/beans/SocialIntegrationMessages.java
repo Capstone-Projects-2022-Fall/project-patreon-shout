29f7cd44e71685b873015582fdce868d6025362a
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-11-21 15:34:21 -0500

    PS-140: Moved social integration messages to unique entity and DB table

---
+++ gateway/src/main/java/com/patreonshout/beans/SocialIntegrationMessages.java
@@ -0,0 +1,34 @@
+package com.patreonshout.beans;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * POJO that relates to the social_integration_messages table in our database
+ */
+@Data
+@Entity
+@Table(name = "social_integration_messages")
+public class SocialIntegrationMessages implements Serializable {
+
+	/**
+	 * PK, contains an FK to webaccounts.webaccount_id
+	 */
+	@Id
+	@Column(name="webaccount_id")
+	protected int webAccountId;
+
+	/**
+	 * Custom Discord message for public Patreon posts
+	 */
+	@Column(name = "discord_public_message")
+	protected String discordMessage;
+
+	/**
+	 * Custom Discord message for private Patreon posts
+	 */
+	@Column(name = "discord_private_message")
+	protected String discordPrivateMessage;
+}
