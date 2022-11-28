846aa6c1908347fbe3646f9ae15a2970c22c384d
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-11-28 14:41:14 -0500

    PS-149: Added EditDisplayMessage documentation

--- frontend/src/outreach_page/EditDisplayMessage.js
+++ frontend/src/outreach_page/EditDisplayMessage.js
@@ -1,5 +1,13 @@
 import reactStringReplace from "react-string-replace";
 
+/**
+ * Edits a given message to fit the PatreonShout standards of displaying messages
+ *
+ * @param givenMessage raw message typed into a text field
+ * @param showLink True if we should replace {link}, false if {link} should simply be removed from the message
+ * @returns {React.ReactNodeArray}
+ * @constructor
+ */
 function EditDisplayMessage(givenMessage, showLink) {
     const exampleMessage = "This is example text for a Patreon post!  When you create a new Patreon post, all of its text will be put here."
     const exampleLink = "https://www.patreon.com/posts/example-post-194817310"
