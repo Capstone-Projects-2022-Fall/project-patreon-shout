a3c6c7e2b35c3cb673b97fcf18d1dc3da761d94d
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-11-28 08:05:17 -0500

    PS-138: Removed duplicate code from ImageSvc

--- gateway/src/main/java/com/patreonshout/rest/ImageSvc.java
+++ gateway/src/main/java/com/patreonshout/rest/ImageSvc.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.image.BufferedImage;
-
 @RestController
 public class ImageSvc extends BaseSvc {
 
@@ -28,24 +26,5 @@ public class ImageSvc extends BaseSvc {
 				.status(HttpStatus.OK)
 				.contentType(MediaType.IMAGE_PNG)
 				.body(ImageUtil.GenerateBlurredImage(imageUrl, radius, text, textColor));
-	}
-
-	// * Source: https://stackoverflow.com/questions/9417356/bufferedimage-resize
-	public static BufferedImage scaleImage(BufferedImage src, int w, int h) {
-		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-		int x, y;
-		int ww = src.getWidth();
-		int hh = src.getHeight();
-		int[] ys = new int[h];
-		for (y = 0; y < h; y++)
-			ys[y] = y * hh / h;
-		for (x = 0; x < w; x++) {
-			int newX = x * ww / w;
-			for (y = 0; y < h; y++) {
-				int col = src.getRGB(newX, ys[y]);
-				img.setRGB(x, y, col);
-			}
-		}
-		return img;
 	}
 }
